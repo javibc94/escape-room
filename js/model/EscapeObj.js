0a4407d04cbac4787a3d411a2be6abedfbef0a05
pol94v <pol.vallverdu@gmail.com>
2017-02-17 09:00:07 +0100

    EscapeObj añadido

---
+++ js/model/EscapeObj.js
@@ -0,0 +1,94 @@
+function reservationObj ()
+{
+	//Attributes declaration
+	this.name;
+	this.type;
+	this.numOfAdults;
+	this.numOfChildren;
+	this.specialRequests = new Array();
+	this.totalPrice;
+
+
+
+	//Methods declaration
+	this.construct = function (id,name,surname,firstAddress,secondAddress, city, state, postalCode, numOfAdults, numOfChildren, phone, email, roomPreference, checkInDate, checkOutDate, checkInTime, checkOutTime, specialRequests, specialInstructions, totalPrice)
+	{
+		this.setName(name);
+		this.setType(type);
+		this.setNumOfAdults(numOfAdults);
+		this.setNumOfChildren(numOfChildren);
+		this.setSpecialRequests(specialRequests);
+		this.setTotalPrice(totalPrice);
+
+	}
+
+	this.setName = function (name){this.name=name;}
+	this.setType = function (type){this.type=type;}
+	this.setNumOfAdults = function (numOfAdults){this.numOfAdults=numOfAdults;}
+	this.setNumOfChildren = function (numOfChildren){this.numOfChildren=numOfChildren;}
+	this.setSpecialRequests = function (specialRequests){this.specialRequests=specialRequests;}
+	this.setTotalPrice = function (totalPrice){this.totalPrice=totalPrice;}
+
+	this.addSpecialRequests = function (specialreq)
+	{
+		this.specialRequests.push(specialreq);
+	}
+
+	this.removeSpecialRequests = function (specialreq)
+	{
+		for (var i = 0; i < this.getSpecialRequests().length; i++)
+		{
+			if(this.getSpecialRequests()[i]==specialreq)
+			{
+				this.specialRequests.splice(i,1);
+				break;
+			}
+		}
+
+	}
+
+	this.getName = function () {return this.name;}
+	this.getType = function () {return this.surname;}
+	this.getNumOfAdults = function () {return this.numOfAdults;}
+	this.getNumOfChildren = function () {return this.numOfChildren;}
+	this.getSpecialRequests = function () {return this.specialRequests;}
+	this.getTotalPrice = function () {return this.totalPrice;}
+
+	this.validate = function ()
+	{
+		var errors = new Array();
+
+		try
+		{
+			if(this.getName().length == 0 || this.getName().match(/^[A-ZÑ][a-zA-Zñá-ú\s]+$/)==null)
+			{
+				errors.push("Name must be informed and contain only letters");
+			}
+		}
+		catch(e) {
+			errors.push("Name must be informed and contain only letters");
+		}
+
+		try
+		{
+			if(this.getType().length == 0)
+			{
+				errors.push("Type must be informed and contain only letters");
+			}
+		}
+		catch(e) {
+			errors.push("Type must be informed and contain only letters");
+		}
+
+		return errors;
+	}
+
+	this.toString = function (){
+		var reservationString ="RESERVATION - FIRST NAME="+this.getName()+" TYPE="+this.getType();
+		reservationString +=" ADULTS="+this.getNumOfAdults();
+		reservationString +=" CHILDREN="+this.getNumOfChildren();
+		reservationString +=" SPECIAL INS="+this.getSpecialInstructions();
+		reservationString +=" TOTAL PRICE="+this.getTotalPrice();
+		return reservationString;
+	}
+}
