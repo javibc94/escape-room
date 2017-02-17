5a5aed442b08280c6479e3410e88c634bfc85f74
Julian David <jdmelgar3jo@hotmail.es>
2017-02-17 10:55:11 +0100

    userObj added

---
+++ js/controller/mainController.js
@@ -0,0 +1,14 @@
+
+// jQuery code
+$(document).ready(function () {
+
+});
+
+//Angular code
+(function (){
+  angular.module('REApp').controller('mainController', ['$scope', '$window', function($scope, $window) {
+    $scope.showAction = 0;
+	}]);
+
+
+})();
---
+++ js/model/userObj.js
@@ -0,0 +1,30 @@
+function userObj ()
+{
+	//Attributes declaration
+	this.name;
+	this.lastName;
+	this.email;
+	this.phone;
+	this.dni;
+
+	//Methods declaration
+	this.construct = function (name, lastName, email, phone, dni) {
+		this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.dni = dni;
+	}
+
+	this.setName = function (name){this.name=name;}
+	this.setLastName = function (lastName){this.lastName=lastName;}
+	this.setEmail = function (email){this.email=email;}
+	this.setPhone = function (phone){this.phone=phone;}
+	this.setDni = function (dni){this.dni=dni;}
+
+	this.getName = function () {return this.name;}
+	this.getLastName = function () {return this.lastName;}
+	this.getEmail = function () {return this.email;}
+	this.getPhone = function () {return this.phone;}
+	this.getDni = function () {return this.dni;}
+}
