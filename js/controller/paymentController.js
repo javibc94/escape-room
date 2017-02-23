aff142191340abc832bc7e6532b8b9f0f94e4d17
pol94v <pol.vallverdu@gmail.com>
2017-02-23 11:28:18 +0100

    version 4, paymentController + paymentForm incomplete

---
+++ js/controller/paymentController.js
@@ -0,0 +1,35 @@
+// jQuery code
+$(document).ready(function () {
+
+});
+
+//Angular code
+(function (){
+	angular.module('REApp').controller('paymentController', ['$scope', '$window', function($scope, $window) {
+		//Properties
+		$scope.user = new paymentObj();
+
+
+		//Scope variables
+		$scope.action;
+
+		this.submit = function() {
+			//$scope.user.$setPristine();
+      //launches the popup with the bill
+			$scope.$parent.showAction = 3;
+		};
+
+
+	}]);
+
+	angular.module('REApp').directive("paymentForm", function (){
+		return {
+			restrict: 'E',
+			templateUrl:"view/templates/payment-form.html",
+			controller:function(){
+
+			},
+			controllerAs: 'paymentForm'
+		};
+	});
+})();
